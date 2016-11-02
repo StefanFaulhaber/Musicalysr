1d333a9e827bbbe4b3b857b4ac26b83a08d09ded
Kevin Dreßler <kvndrsslr@gmail.com>
2016-11-02 15:31:46 +0100

    fixed commented lines

--- vagrant/setup.sh
+++ vagrant/setup.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 cd /opt/dev/frontend/
-#NPM=-1
-#GUARD=5
-#COUNTER=1
-#rm -rf node_modules/
-#until [ ${NPM} -eq 0 ] || [ ${COUNTER} -gt ${GUARD} ]; do
-#         sudo npm i --no-bin-links
-#         let NPM=$?
-#         let COUNTER+=1
-#done
+NPM=-1
+GUARD=5
+COUNTER=1
+rm -rf node_modules/
+until [ ${NPM} -eq 0 ] || [ ${COUNTER} -gt ${GUARD} ]; do
+         sudo npm i --no-bin-links
+         let NPM=$?
+         let COUNTER+=1
+done
 tmux new-session -d -s frontend-serve 'ng serve --host 0.0.0.0'
 #tmux detach -s my_session
