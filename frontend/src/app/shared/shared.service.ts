c3138a2590042eb898d4c0d9eb4202c8ab33ae0e
Stefan <stefan.guesten@gmx.de>
2017-01-06 19:42:22 +0100

    added settings (sorry for the hack), fixed some minor bugs

--- frontend/src/app/shared/shared.service.ts
+++ frontend/src/app/shared/shared.service.ts
@@ -7,6 +7,11 @@ import { Artist } from '../models/artist';
 @Injectable()
 export class SharedService {
 
+  isWikipediaEnabled: boolean = true;
+  isDiscographyEnabled: boolean = true;
+  isPopularityEnabled: boolean = true;
+  isYoutubeEnabled: boolean = true;
+
   artist: Artist = new Artist();
   artistSource = new BehaviorSubject<Artist>(this.artist);  // Observable item source
   artistItem = this.artistSource.asObservable();            // Observable item stream
