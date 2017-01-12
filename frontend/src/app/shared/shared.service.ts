8356e9a1d9c708097f5cdd1afdea48382dc1e3c8
Stefan <stefan.guesten@gmx.de>
2017-01-12 13:17:23 +0100

    write settings to localStorage

--- frontend/src/app/shared/shared.service.ts
+++ frontend/src/app/shared/shared.service.ts
@@ -16,8 +16,26 @@ export class SharedService {
   artistSource = new BehaviorSubject<Artist>(this.artist);  // Observable item source
   artistItem = this.artistSource.asObservable();            // Observable item stream
 
+  constructor() {
+    if (localStorage.getItem('isWikipediaEnabled'))
+      this.isWikipediaEnabled = (localStorage.getItem('isWikipediaEnabled') === 'true');
+    if (localStorage.getItem('isDiscographyEnabled'))
+      this.isDiscographyEnabled = (localStorage.getItem('isDiscographyEnabled') === 'true');
+    if (localStorage.getItem('isPopularityEnabled'))
+      this.isPopularityEnabled = (localStorage.getItem('isPopularityEnabled') === 'true');
+    if (localStorage.getItem('isYoutubeEnabled'))
+      this.isYoutubeEnabled = (localStorage.getItem('isYoutubeEnabled') === 'true');
+  }
+
   // service command
   changeArtist(a: Artist) {
     this.artistSource.next(a);
+  }
+
+  writeSettings() {
+    localStorage.setItem('isWikipediaEnabled', this.isWikipediaEnabled.toString());
+    localStorage.setItem('isDiscographyEnabled', this.isDiscographyEnabled.toString());
+    localStorage.setItem('isPopularityEnabled', this.isPopularityEnabled.toString());
+    localStorage.setItem('isYoutubeEnabled', this.isYoutubeEnabled.toString());
   }
 }
